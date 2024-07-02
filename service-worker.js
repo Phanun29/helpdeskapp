bac39cc3bdaef99f6130882ae466b4b8662c9dd6
Phanun29 <nenzigaming@gmail.com>
2024-07-02 14:32:53 +0700

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/my-web-app/',
+  '/my-web-app/styles/main.css',
+  '/my-web-app/scripts/app.js',
+  '/my-web-app/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
